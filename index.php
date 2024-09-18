146b53eb135c862d7c55b034964060331ed29fcd
kar-iuki <ikariukiirungu@gmail.com>
2024-09-18 15:08:06 +0300

    index file

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<?php
+//require_once "load.php";
+//print $Obj->user_age("Alex", 2004);
 
+//public function about_content(){
+
+//}
+
+require "load.php";
+$ObjLayouts->heading();
+$ObjMenus->main_menu();
+$ObjHeadings-> main_banner();
+$ObjCont->main_content();
+$ObjCont->main_right_menu();
+$ObjCont->side_bar();
+$ObjLayouts->footer();
+
+?>
