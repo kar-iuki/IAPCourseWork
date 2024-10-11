faa9a37158902417392d53dc2b72032432feabb7
kar-iuki <ikariukiirungu@gmail.com>
2024-10-11 19:18:21 +0300

    sign up form

--- signUp.php
+++ signUp.php
@@ -2,7 +2,8 @@
 require "load.php";
 $ObjLayouts->heading();
 $ObjMenus->main_menu();
-$ObjHeadings->main_banner();
-$ObjCont->sign_up();
+$ObjHeadings-> main_banner();
+$ObjForm->sign_up_form($ObjGlob);
+$ObjCont->side_bar();
 $ObjLayouts->footer();
 ?>
