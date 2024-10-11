4a4cd6bf6cad2e9f67220a6913dd4382860a2003
kar-iuki <ikariukiirungu@gmail.com>
2024-10-11 21:03:34 +0300

    addition of functions

--- includes/dbConnection.php
+++ includes/dbConnection.php
@@ -42,4 +42,159 @@
                     break;
             }
         }
+
+
+        public function escape_values($posted_values): string{
+            switch ($this->db_type) {
+                case 'PDO':
+                    $this->posted_values = addslashes($posted_values);
+                    break;
+                case 'MySQLi':
+                    $this->posted_values = $this->connection->real_escape_string($posted_values);
+                    break;
+            }
+        return $this->posted_values;
+        }
+
+        
+        public function count_results($sql){
+            switch ($this->db_type) {
+                case 'PDO':
+                    $res = $this->connection->prepare($sql);
+                    $res->execute();
+                    return $res->rowCount();
+                    break;
+                case 'MySQLi':
+                    if(is_object($this->connection->query($sql))){
+                        $result = $this->connection->query($sql);
+                        return $result->num_rows;
+                    }else{
+                        print "Error 5: " . $sql . "<br />" . $this->connection->error . "<br />";
+                    }
+                    break;
+            }
+        }
+
+
+        public function insert($table, $data){
+            ksort($data);
+            $fieldDetails = NULL;
+            $fieldNames = implode('`, `',  array_keys($data));
+            $fieldValues = implode("', '",  array_values($data));
+            $sth = "INSERT INTO $table (`$fieldNames`) VALUES ('$fieldValues')";
+            return $this->extracted($sth);
+        }
+
+
+        public function select($sql){
+            switch ($this->db_type) {
+                case 'PDO':
+                    $result = $this->connection->prepare($sql);
+                    $result->execute();
+                    return $result->fetchAll(PDO::FETCH_ASSOC)[0];
+                    break;
+                case 'MySQLi':
+                    $result = $this->connection->query($sql);
+                    return $result->fetch_assoc();
+                    break;
+            }
+        }
+
+
+        public function select_while($sql){
+            switch ($this->db_type) {
+                case 'PDO':
+                    $result = $this->connection->prepare($sql);
+                    $result->execute();
+                    return $result->fetchAll(PDO::FETCH_ASSOC);
+                    break;
+                case 'MySQLi':
+                    $result = $this->connection->query($sql);
+                    for ($res = array (); $row = $result->fetch_assoc(); $res[] = $row);
+                    return $res;
+                    break;
+            }
+        }
+
+
+        public function update($table, $data, $where){
+            $wer = '';
+            if(is_array($where)){
+                foreach ($where as $clave=>$value){
+                    $wer.= $clave."='".$value."' AND ";
+                }
+                $wer   = substr($wer, 0, -4);
+                $where = $wer;
+            }
+            ksort($data);
+            $fieldDetails = NULL;
+            foreach ($data as $key => $values){
+                $fieldDetails .= "$key='$values',";
+            }
+            $fieldDetails = rtrim($fieldDetails,',');
+            if($where==NULL or $where==''){
+                $sth = "UPDATE $table SET $fieldDetails";
+            }else {
+                $sth = "UPDATE $table SET $fieldDetails WHERE $where";
+            }
+            return $this->extracted($sth);
+        }
+
+
+        public function delete($table,$where){
+            $wer = '';
+            if(is_array($where)){
+                foreach ($where as $clave=>$value){
+                    $wer.= $clave."='".$value."' and ";
+                }
+                $wer   = substr($wer, 0, -4);
+                $where = $wer;
+            }
+            if($where==NULL or $where==''){
+                $sth = "DELETE FROM $table";
+            }else{
+                $sth = "DELETE FROM $table WHERE $where";
+            }
+                return $this->extracted($sth);
+        }
+
+
+        public function truncate($table){
+            $sth = "TRUNCATE $table";
+            return $this->extracted($sth);
+        }
+
+
+        public function last_id(){
+            switch ($this->db_type) {
+            case 'PDO':
+                    return $this->connection->lastInsertId();
+                break;
+            case 'MySQLi':
+                return $this->connection->insert_id;
+            break;
+            }
+        }	
+
+
+        public function extracted(string $sth){
+            switch ($this->db_type) {
+                case 'PDO':
+                    try {
+                        $stmt = $this->connection->prepare($sth);
+                        $stmt->execute();
+                        return TRUE;}
+                    catch (PDOException $e) {
+                        return $sth . "<br>" . $e->getMessage();
+                        }
+                    break;
+                case 'MySQLi':
+                if ($this->connection->query($sth) === TRUE) {
+                    return TRUE;
+                } else {
+                    return "Error: " . $sth . "<br>" . $this->connection->error;
+                }
+                break;
+            }
+        }
     }
