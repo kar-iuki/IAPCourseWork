7f6ae4d540afa7a7b9c9d93f8c33184391ec9d40
kar-iuki <ikariukiirungu@gmail.com>
2024-10-11 19:17:24 +0300

    headers with bootstrap

--- contents/headings.php
+++ contents/headings.php
@@ -2,9 +2,13 @@
 class headings{
     public function main_banner(){
         ?>
-        <div class="banner">
-            <h1>Heading</h1>
-    </div>
+<div class="p-5 mb-4 bg-body-tertiary rounded-3">
+        <div class="container-fluid py-2">
+          <h1 class="display-5 fw-bold">Assignment II</h1>
+          <p class="col-md-12 fs-6">This assignment is as given on E-learning</p>
+          <button class="btn btn-primary btn-lg" type="button">Link to github</button>
+        </div>
+      </div>
     <?php
     }
 }
