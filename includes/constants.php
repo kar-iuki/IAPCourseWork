9116ce25b23cb1aadd2ba8c8ea1e5570bc63b7fd
kar-iuki <ikariukiirungu@gmail.com>
2024-10-11 19:34:32 +0300

    loading the constants

--- includes/constants.php
+++ includes/constants.php
@@ -1,4 +1,6 @@
 <?php
+date_default_timezone_set("AFRICA/Nairobi");
+
     //Creating constants
     define('DBTYPE', 'PDO');
     define('HOSTNAME', '127.0.0.1');
@@ -6,3 +8,12 @@
     define('HOSTUSER', 'root');
     define('HOSTPASS', '');
     define('DBNAME', 'api_d');
+
+    $protocol = isset($_SERVER['HTTPS']) && 
+    $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+    $base_url = $protocol . $_SERVER['HTTP_HOST'] . '/';
+
+    $conf['ver_code_time'] = date("Y-m-d H:i:s", strtotime("+ 24hours"));
+    $conf['verification_code'] = rand(100000,999999);
+    $conf['site_initials'] = "ICS 2024";
+    $conf['site_url'] = "$base_url/". DBNAME;
