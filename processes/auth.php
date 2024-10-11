95b8e751c914d4d81924db6045cfefdcaf83a1ba
kar-iuki <ikariukiirungu@gmail.com>
2024-10-11 19:26:43 +0300

    plugins

--- processes/auth.php
+++ processes/auth.php
@@ -16,9 +16,6 @@ class auth{
             $email_address = $_SESSION["email_address"] = $conn->escape_values(strtolower($_POST["email_address"]));
             $username = $_SESSION["username"] = $conn->escape_values(strtolower($_POST["username"]));
 
-// Implement input validation and error handling
-// =============================================
-// Sanitize all inputs
 
 // verify that the fullname has only letters, space, dash, quotation
 if(ctype_alpha(str_replace(" ", "", str_replace("\'", "", $fullname))) === FALSE){
@@ -59,13 +56,10 @@ if (!ctype_alpha($username)) {
     $errors['usernameLetters_err'] = "Invalid username format. Username must contain letters only";
 }
 
-// Verify that the password is identical to the repeat passsword
-// verify that the password length is between 4 and 8 characters
+
 if(!count($errors)){
 
-// Implement 2FA (email => PHP-Mailer)
-// ===================================
-// Send email verification with an OTP (OTC)
+
 
 
             $cols = ['fullname', 'email', 'username', 'ver_code', 'ver_code_time'];
